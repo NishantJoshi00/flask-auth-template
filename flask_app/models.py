d112ff6d3b463f5bea7804dbc514ec2f5f0eca6f
Nishant Joshi <nishantjo.12@gmail.com>
2020-08-19 12:08:36 +0530

    Webpage

---
+++ flask_app/models.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(100))
+	email = db.Column(db.String(100), unique=True)
+	password = db.Column(db.String(100))
